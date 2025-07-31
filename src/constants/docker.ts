2c37b80ee294180629ef012352a772ece8b77be9
shu-unifra <shu@unifra.io>
2025-08-01 08:36:15 +0900

    contracts tag to skip-l1-deploy

--- src/constants/docker.ts
+++ src/constants/docker.ts
@@ -2,4 +2,4 @@ export const DOCKER_REPOSITORY = 'dogeos69/scroll-stack-contracts'
 
 // Base URL for Docker Hub v2 API for the above repository
 export const DOCKER_TAGS_URL = `https://registry.hub.docker.com/v2/repositories/${DOCKER_REPOSITORY}/tags`; 
-export const DOCKER_DEFAULT_TAG = '20250731-00'
+export const DOCKER_DEFAULT_TAG = 'skip-l1-deploy'
