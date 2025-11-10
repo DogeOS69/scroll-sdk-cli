c29f7ada2d27e300eede1c1756aa9a17874c9aa2
shu-unifra <shu@unifra.io>
2025-11-11 02:48:58 +0900

    update to 0.2.0-rc.1

--- src/commands/doge/bridge-init.ts
+++ src/commands/doge/bridge-init.ts
@@ -45,7 +45,7 @@ export class BridgeInitCommand extends Command {
     }
 
     private async getDockerImageTag(providedTag: string | undefined): Promise<string> {
-        const defaultTag = '0.2.0-rc.0-71a4fcb4'
+        const defaultTag = '0.2.0-rc.1'
 
         if (!providedTag) {
             return defaultTag
